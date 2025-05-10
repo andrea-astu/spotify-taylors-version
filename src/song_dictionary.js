0c7ab2cd285af3fd02669cd37c7275c671e0d885
andrea-astu <162191015+andrea-astu@users.noreply.github.com>
2025-05-10 18:19:45 +0200

    song hash table now in a separate file

---
+++ src/song_dictionary.js
@@ -0,0 +1,219 @@
+// hash table for song IDs
+export const song_table = {
+    // fearless (international version)
+    "6zV8AVM9eVfdG7Qahwd64R" : "77sMIMlNaSURUAXq5coCxE", // fearless
+    "4yf9OivCH2lmQWN8mzpCC6" : "2nqio0SfWg6gh2eCtfuMa5", // fifteen
+    "5xPsybHWRHbTYO44xnREY5" : "6YvqWjhGD8mB5QXcbcUKtx", // love story
+    "4fWDgRZa0GKE5KZNhE9MQa" : "550erGcdD9n6PnwxrvYqZT", // hey stephen
+    "1yFjBRQVhRdcdnrTE5LuyD" : "5YL553x8sHderRBDlm3NM3", // white horse
+    "7G6TDmGJXDoBaV6NGj3Yic" : "1qrpoAMXodY6895hGKoUpA", // you belong with me
+    "0qsSmObOgBZfuMIwfAQDBP" : "7HC7R2D8WjXVcUHJyEGjRs", // breathe
+    "6vLpy7UPYuOa75Bkqocrcw" : "0k0vFacOHNuArLWMiH60p7", // tell me why
+    "4N419mmxYybTfAECy5j4dw" : "6iiAfo4wTA2CVC3Uwx9uh8", // you're not sorry
+    "04LDcz3RX1P5sIlzQGqgUN" : "22bPsP2jCgbLUvh82U0Z3M", // the way i loved you
+    "3yuOL0jFnVR8tizZzNNo0F" : "1msEuwSBneBKpVCZQcFTsU", // forever & always
+    "7G9D32Kx7hdQ9Uga1V4VXm" : "6ON9UuIq49xXY9GPmHIYRp", // the best day
+    "493jXNrYmXPCMbSJvVRYLk" : "3ExweHKZF9B752DPQByRVT", // change
+
+    // fearless (big machine radio)
+    "15PRxID4aGtOncbffzgZXI" : "77sMIMlNaSURUAXq5coCxE", // fearless
+    "6aQrB8kaWyaCj9RYB7Rh5L" : "2nqio0SfWg6gh2eCtfuMa5", // fifteen
+    "1D4PL9B8gOg78jiHg3FvBb" : "6YvqWjhGD8mB5QXcbcUKtx", // love story
+    "3DTqKBlJPlRSclFfBI0ZoE" : "550erGcdD9n6PnwxrvYqZT", // hey stephen
+    "4QhjEJ2o6QNwpZGs51AmiY" : "5YL553x8sHderRBDlm3NM3", // white horse
+    "1GEBsLDvJGw7kviySRI6GX" : "1qrpoAMXodY6895hGKoUpA", // you belong with me
+    "2dzk2z7MXl0peRhBUsDq4d" : "7HC7R2D8WjXVcUHJyEGjRs", // breathe
+    "7bUvIh9rQsboJBXaSIQZAF" : "0k0vFacOHNuArLWMiH60p7", // tell me why
+    "0ReNbPVMU7jUWqn1yHo9cE" : "6iiAfo4wTA2CVC3Uwx9uh8", // you're not sorry
+    "54ziHHVIBJg3YJ9sn17mEy" : "22bPsP2jCgbLUvh82U0Z3M", // the way i loved you
+    "5VHdgl9hFUmabiCO5jeISv" : "1msEuwSBneBKpVCZQcFTsU", // forever & always
+    "0ck1qErO76fM9xKxuSeqDk" : "6ON9UuIq49xXY9GPmHIYRp", // the best day
+    "6nIUWFgMly8cdh32SbnhiR" : "3ExweHKZF9B752DPQByRVT", // change
+
+    // fearless (platinum version)
+    "6WWvcA62QlnRguluWAk6bv" : "2m3ObD945KvpE5y9A1eUWm", // jump then fall
+    "6ovFJkdau79DuXEvQjPaNH" : "0tQ9vBYpldCuikPsbgOVKA", // untouchable
+    "13MAWVUSkbGMUi6I7RowRK" : "01QdEx6kFr78ZejhQtWR5m", // forever & always (piano version)
+    "0cEG04R5cldkTLzK6phlcn" : "1n2wszmJyVkw6FHqyLnQsY", // come in with the rain
+    "0XfTh16YcKPzdxMPTeKTAq" : "51A8eKvvZz9uydvIZ7xRSV", // superstar
+    "1yDJcXc549N6AabNBzFAku" : "1cSFlSBdpT4F5vb1frQ231", // the other side of the door
+    "2okho7vU7Nsq1UZD0kgIMi" : "77sMIMlNaSURUAXq5coCxE", // fearless
+    "5Ora3Ish2A1JN5veGYTxhw" : "2nqio0SfWg6gh2eCtfuMa5", // fifteen
+    "5hnJCRSo4pxRsaEwT9YusG" : "6YvqWjhGD8mB5QXcbcUKtx", // love story
+    "3CI1f4yLwdHzcUnH7o0Xyh" : "550erGcdD9n6PnwxrvYqZT", // hey stephen
+    "5yKB8qcTSflDercM2PXwot" : "5YL553x8sHderRBDlm3NM3", // white horse
+    "4kgTdThcDHfuDS2kKxB7Lc" : "1qrpoAMXodY6895hGKoUpA", // you belong with me
+    "23qIWGhpwvDgC23TyZOUck" : "7HC7R2D8WjXVcUHJyEGjRs", // breathe
+    "4x7Gxk2GfwZJcqpoVT1PbR" : "0k0vFacOHNuArLWMiH60p7", // tell me why
+    "3JhvYnOfKLJuWC7jEU0GmJ" : "6iiAfo4wTA2CVC3Uwx9uh8", // you're not sorry
+    "4pEZUytwBzha5Un6m3SCFf" : "22bPsP2jCgbLUvh82U0Z3M", // the way i loved you
+    "3NKCWBT2uKp8rxSkcvIrdX" : "1msEuwSBneBKpVCZQcFTsU", // forever & always
+    "7CjuAoOiSrwWv7wJonAUmw" : "6ON9UuIq49xXY9GPmHIYRp", // the best day
+    "3gFaNwyiafgUoQhR0pHYA9" : "3ExweHKZF9B752DPQByRVT", // change
+
+    // today was a fairytale
+    "4pFvEWbjBpPUdYRQly0THs" : "2JoJrsEV15OzbijS47lids",
+
+    // speak now
+    "0GxW5K0qzrq7L1jwSY5OmY" : "7G0gBu6nLdhFDPRLc0HdDG", // mine (pop mix)
+    "46NGAvL695VnbcSeTmQWmM" : "3MytWN8L7shNYzGl4tAKRp", // sparks fly
+    "5l6GqSxplqnZbLg0LTPQeG" : "79uDOz0zuuWS7HWxzMmTa2", // back to december
+    "7dXKdjpOaH2iwfFKmUj7aq" : "5xXqyjLicvEpch72qEryFT", // speak now
+    "3K2es3gElMRJ3qvmPW442g" : "1zU8j1x3yi9xalMF96pzKp", // dear john
+    "0KztjtD3ytzgYjUKIVfBnW" : "30Y4CV7A6YqtQtTTo7Ue4j", // mean
+    "3lXekiVK1ZPZbKconsys73" : "6dTA6y0C2ReQklntzZl8l3", // the story of us
+    "3JilapcaOXzjR9gtvCY8FO" : "2EFZ9emtKWEglWUQGEQ3P9", // never grow up
+    "04S1pkp1VaIqjg8zZqknR5" : "3sW3oSbzsfecv9XoUdGs7h", // enchanted
+    "0PWgAyoxsjwGRuFSO0fyya" : "0NwGC0v03ysCYINtg6ns58", // better than revenge
+    "0mTgFzkDgSAF3nJY25XCXG" : "12nBPF4Rh4XLFJV0YLN7uj", // innocent
+    "2YjKcFQ1vYFhQPgsmCOjss" : "4tMzIAFTFdqGBQLdfbPces", // haunted
+    "4EUQXLipplO2v9mVaqQtjc" : "59KOoHFcw5XfICnO57holu", // last kiss
+    "4ewAfHYpDTzpW1GKO44CVP" : "4hqJ4bSlYJOXb6Z4SRmzxs", // long live
+
+    // speak now (deluxe)
+    "0OAHsZ8vrS2NKom3cvfJQD" : "7G0gBu6nLdhFDPRLc0HdDG", // mine (pop mix)
+    "09cM9BjyNFizKUOXh6j9rT" : "3MytWN8L7shNYzGl4tAKRp", // sparks fly
+    "0tr6XR58KBdDYd8qvHVTs8" : "79uDOz0zuuWS7HWxzMmTa2", // back to december
+    "2PBghI9zJkQEWBzMSMo2Ki" : "5xXqyjLicvEpch72qEryFT", // speak now
+    "1GeLxMgpeCnMJntwyW6rLw" : "1zU8j1x3yi9xalMF96pzKp", // dear john
+    "6yM6QsnTCTVOkKEvg3hGlo" : "30Y4CV7A6YqtQtTTo7Ue4j", // mean
+    "7vBlnGdfOLzVEqiOQQxeU8" : "6dTA6y0C2ReQklntzZl8l3", // the story of us
+    "57w0Uyk2jJAkO2hMJ36xJZ" : "2EFZ9emtKWEglWUQGEQ3P9", // never grow up
+    "3sqrvkNC6IPTIXvvbx9Arw" : "3sW3oSbzsfecv9XoUdGs7h", // enchanted
+    "55mh9j2aB7xZ1Oh463gK8k" : "0NwGC0v03ysCYINtg6ns58", // better than revenge
+    "4LraIz87diVQdDK0rhi0S4" : "12nBPF4Rh4XLFJV0YLN7uj", // innocent
+    "72idZxdEHLbi2YI486c3sE" : "4tMzIAFTFdqGBQLdfbPces", // haunted
+    "1VCJ7vk3Y2DoJIAEOXe3V8" : "59KOoHFcw5XfICnO57holu", // last kiss
+    "7BFc7ffruhZ4Hecnqf5xju" : "4hqJ4bSlYJOXb6Z4SRmzxs", // long live
+    "0hK93KD6Y0j9giUGh4Valj" : "3yNJkriPzWjkkDAWHIAVUq", // ours
+    "5tSmAABuoOfR59lrtXdDqm" : "0kAZ3H6G9Zac4PMpmobMkj", // if this was a movie
+    "6aJ0ipi4EPmu1aTbFeXZnw" : "4evLyY5Ue1Wesc61t2KXAU", // superman
+    "67Io3gxHwfbUreBf114c0u" : "7G0gBu6nLdhFDPRLc0HdDG", // mine
+    "5aNkiOMxQXkpoEY5bTYoCh" : "79uDOz0zuuWS7HWxzMmTa2", // back to december
+    "2jt90bYlYDg1lXYWES34LJ" : "6dTA6y0C2ReQklntzZl8l3", // the story of us
+
+    // speak now (big machine radio)
+    "3uma4z6bJJxig2eUILcDbp" : "7G0gBu6nLdhFDPRLc0HdDG", // mine
+    "1wJzOxcUulTXGgZEOURumN" : "3MytWN8L7shNYzGl4tAKRp", // sparks fly
+    "6OVJMTfXU9rbYI51AoEuLH" : "79uDOz0zuuWS7HWxzMmTa2", // back to december
+    "0Olj747orZlOhMeaTRSlxc" : "5xXqyjLicvEpch72qEryFT", // speak now
+    "7I3hYiXgQYPLS0HsPF9mrk" : "1zU8j1x3yi9xalMF96pzKp", // dear john
+    "2AyPGjJkLw5i2xBsxDvUQC" : "30Y4CV7A6YqtQtTTo7Ue4j", // mean
+    "7dlofsE0pZhXBZR6hqhaVu" : "6dTA6y0C2ReQklntzZl8l3", // the story of us
+    "1NXRcoa3St2wQxlT9iCAEV" : "2EFZ9emtKWEglWUQGEQ3P9", // never grow up
+    "1nyySeUvIxeIydsDL4G80X" : "3sW3oSbzsfecv9XoUdGs7h", // enchanted
+    "1Ou5KTDC8WTcRYzF6TuCOM" : "0NwGC0v03ysCYINtg6ns58", // better than revenge
+    "0xGGOx0t5rWxX28iZyVWyc" : "12nBPF4Rh4XLFJV0YLN7uj", // innocent
+    "6lz5WeAIlOuCEkl6kqJdaS" : "4tMzIAFTFdqGBQLdfbPces", // haunted
+    "15cELoe9O3O1Wj8APIXdyG" : "59KOoHFcw5XfICnO57holu", // last kiss
+    "5gHX33LEbaLcQoCA5o93JQ" : "4hqJ4bSlYJOXb6Z4SRmzxs", // long live
+
+    // red
+    "786NsUYn4GGUf8AOt0SQhP" : "6lzc0Al0zfZOIFsFvBS1ki", // state of grace
+    "0cITLOYn1Sv4q27zZPqlNK" : "4OAuvHryIVv4kMDNSLuPt6", // red
+    "5kYFVSQoPu7yRpfiHBwMUk" : "3S7HNKPakdwNEBFIVTL6dZ", // treacherous
+    "72jCZdH0Lhg93z6Z4hBjgj" : "6AtZLIzUINvExIUy4QhdjP", // i knew you were trouble
+    "4XMP3zVxrnr58T0tjIHvpR" : "3nsfB1vus2qaloUdcBZvDu", // all too well
+    "2ULNeSomDxVNmdDy8VxEBU" : "3yII7UwgLF6K5zW3xad3MP", // 22
+    "5VwFkx7JOimOGTYfha5rs1" : "2r9CbjYgFhtAmcFv1cSquB", // i almost do
+    "7AEAGTc8cReDqcbPoY9gwo" : "5YqltLsjdqFtvqE7Nrysvs", // we are never ever getting back together
+    "1qLeEu4iXEclkLwoBlMiou" : "7eQj6r5PIdYKEIZjucBMcq", // stay stay stay
+    "3n9mzjgAzjyKawvbHzciXF" : "0y6kdSRCVQhSsHSpWvTUm7", // the last time
+    "1fGsZsIkQkOspfsT24nQP6" : "7J4b3LVCIGO4CMBDFLPoP6", // holy ground
+    "6LKjHhOW1az75pCQ9XJJtF" : "73qMN9bXy7MSPwwGfH3wQr", // sad beautiful tragic
+    "2X2J0BhxaLTmnxO4pPUhSd" : "4e5ayHsOLJNLTGfjau2mEw", // the lucky one
+    "4NAmRvqSITAAzKWnC8yRq3" : "7qEUFOVcxRI19tbT68JcYK", // everything has changed
+    "7MecVsMj22MneZt7kVFaCr" : "7A2cNLRT0YJc1yjxHlKihs", // starlight
+    "0L4YCNRfXAoTvdpWeH2RGj" : "05GsNucq8Bngd9fnd4fRa0", // begin again
+
+    // red (deluxe)
+    "0b16LTzby1YRVd2nq2Z0fw" : "6lzc0Al0zfZOIFsFvBS1ki", // state of grace
+    "7gJtmLyPTwKzhGzMBXtuXH" : "4OAuvHryIVv4kMDNSLuPt6", // red
+    "7nECwCDHS1JvQbrmuorZIH" : "3S7HNKPakdwNEBFIVTL6dZ", // treacherous
+    "3lKZf71jeNi14udIFPJ29V" : "6AtZLIzUINvExIUy4QhdjP", // i knew you were trouble
+    "6GNRkaWUB0Lwc19SdkTgx8" : "3nsfB1vus2qaloUdcBZvDu", // all too well
+    "3WC5CVAahvn98hiseoIvbw" : "3yII7UwgLF6K5zW3xad3MP", // 22
+    "1eyqSYBCTane4rl4vc6PWJ" : "2r9CbjYgFhtAmcFv1cSquB", // i almost do
+    "6kISMRfweKSvgHR0urMQFG" : "5YqltLsjdqFtvqE7Nrysvs", // we are never ever getting back together
+    "0F2S1QlnMaOfmNHIgmES3k" : "7eQj6r5PIdYKEIZjucBMcq", // stay stay stay
+    "2Tz4JnXZOcjcp8fWfMjBBk" : "0y6kdSRCVQhSsHSpWvTUm7", // the last time
+    "3KmYVOuK2eG7LGP7qQ8DLR" : "7J4b3LVCIGO4CMBDFLPoP6", // holy ground
+    "70e9zuv2pJc7ON1OUAJ1zl" : "73qMN9bXy7MSPwwGfH3wQr", // sad beautiful tragic
+    "5VQLMrWhailAhErKNUSnv1" : "4e5ayHsOLJNLTGfjau2mEw", // the lucky one
+    "5PjfMmF06QtxTPZBZHdhoZ" : "7qEUFOVcxRI19tbT68JcYK", // everything has changed
+    "1gL4vMhDV3RgJQmDuY5HK1" : "7A2cNLRT0YJc1yjxHlKihs", // starlight
+    "1yG491M3fRfowMdGN5pey4": "05GsNucq8Bngd9fnd4fRa0", // begin again
+    "4qTAwKJQ68gmnrnfPD3vlO" : "0NRHj8hDwwmSPaA41o379r", // the moment i knew
+    "1n8rB9AhxiAvVP1E2c3yOd" : "4pNApnaUWAL2J4KO2eqokq", // come back, be here
+    "6VFpuI0DDmbC5UyWZQ4IRY" : "0DMVrlMUn01M0IcpDbwgu7", // girl at home
+    "5mfDBWY7ncZFm7ykmFTG4i" : "5jAIouBES8LWMiriuNq170", // state of grace (acoustic)
+
+    // red (big machine radio)
+    "5gl2F1AWDQlUFzXwA4uY72" : "6lzc0Al0zfZOIFsFvBS1ki", // state of grace
+    "4yBEU0askBZzE2XEVdEwmx" : "4OAuvHryIVv4kMDNSLuPt6", // red
+    "6xlCHgsZanRIBaS3yokshA" : "3S7HNKPakdwNEBFIVTL6dZ", // treacherous
+    "6FB3v4YcR57y4tXFcdxI1E" : "6AtZLIzUINvExIUy4QhdjP", // i knew you were trouble
+    "7HH8rEmkK5JENnv869MDCz" : "3nsfB1vus2qaloUdcBZvDu", // all too well
+    "6ce41JVa6KFaryfmC2bu2W" : "3yII7UwgLF6K5zW3xad3MP", // 22
+    "4Ar3jciiuPXAMGjQpMWo34" : "2r9CbjYgFhtAmcFv1cSquB", // i almost do
+    "0VwNdo84DaVYLIkbVO86ND" : "5YqltLsjdqFtvqE7Nrysvs", // we are never ever getting back together
+    "5A4PQqcpYb0vD1zx5DlObS" : "7eQj6r5PIdYKEIZjucBMcq", // stay stay stay
+    "4L9QDCVNWIFhf2yr8iMvzA" : "0y6kdSRCVQhSsHSpWvTUm7", // the last time
+    "5iZzl5YlcJaEZjFCcv9j2h" : "7J4b3LVCIGO4CMBDFLPoP6", // holy ground
+    "5qD1xj5lXOaX1ZDpVKZeF3" : "73qMN9bXy7MSPwwGfH3wQr", // sad beautiful tragic
+    "3m4mW6nG5V94FkGBE1YvRG" : "4e5ayHsOLJNLTGfjau2mEw", // the lucky one
+    "3Z1kZKYfRC8iRXnYeC5sCJ" : "7qEUFOVcxRI19tbT68JcYK", // everything has changed
+    "2D1BGzlocoxwNBBf2nMwMk" : "7A2cNLRT0YJc1yjxHlKihs", // starlight
+    "4uoyvFGK43Of4v9tj0moxC": "05GsNucq8Bngd9fnd4fRa0", // begin again
+
+    // 1989
+    "10nqz67NQWWa7XPq7ycihi" : "4WUepByoeqcedHoYhSNHRt", // welcome to ny
+    "2ls70nUDfjzm1lSRDuKxmw" : "0108kcWLnn2HlH2kedi1gn", // blank space
+    "71PmZqBXH0RUETqxpwlV0w" : "3Vpk1hfMAQme8VJ0SNRSkd", // style
+    "7EpVQjqtKygiwJIASxMWNg" : "1OcSfkeCg9hRC2sFKB4IMJ", // out of the woods
+    "4NNolUeL5m2ju7kf8pdg6H" : "2k0ZEeAqzvYMcx9Qt5aClQ", // all you had to do was stay
+    "1kKlhuQE0HXp1IwBRpaH2P" : "50yNTF0Od55qnHLxYsA5Pw", // shake it off
+    "5RVJ6VKfwtosA5Q1qeHdjF" : "3FxJDucHWdw6caWTKO5b23", // i wish you would
+    "0TvQLMecTE8utzoNmvXRbK" : "7oZONwFiFIErZcXAtTu7FY", // bad blood
+    "3fVnlF4pGqWI9flVENcT28" : "27exgla7YBw9DUNNcTIpjy", // wildest dreams
+    "5Z9ocF6ng0m2haHnKtHwTp" : "733OhaXQIHY7BKtY3vnSkn", // how you get the girl
+    "7lTxQZVXlvIX7Z3szSy8Sh" : "4WBEj8TeGtRPNJdOmT3WJW", // this love
+    "6Ck9WerfsyvC4v4AylUoDp" : "1ULabO0PEsdJekqVH6G10G", // i know places
+    "2k43eySbfpDVx0PrO99UI8" : "0lKUBmEyQfzsQHozyeXzES", // clean
+
+    // 1989 (deluxe)
+    "3nRmDz7qGCvsMS30rGGY0x" : "4WUepByoeqcedHoYhSNHRt", // welcome to ny
+    "1u8c2t2Cy7UBoG4ArRcF5g" : "0108kcWLnn2HlH2kedi1gn", // blank space
+    "0ug5NqcwcFR2xrfTkc7k8e" : "3Vpk1hfMAQme8VJ0SNRSkd", // style
+    "0qUnBLZ8bJqUNEeQgayL9t" : "1OcSfkeCg9hRC2sFKB4IMJ", // out of the woods
+    "6aLOekfwbytwWvQftxTEF0" : "2k0ZEeAqzvYMcx9Qt5aClQ", // all you had to do was stay
+    "0cqRj7pUJDkTCEsJkx8snD" : "50yNTF0Od55qnHLxYsA5Pw", // shake it off
+    "5gRYrtvyVyaCRvLt56OfuV" : "3FxJDucHWdw6caWTKO5b23", // i wish you would
+    "2NlmmAjGYrrjAp0MED5rGx" : "7oZONwFiFIErZcXAtTu7FY", // bad blood
+    "4A4A6dwl5DqdqrZaEtKGPR" : "27exgla7YBw9DUNNcTIpjy", // wildest dreams
+    "0fM9dEhUFV4MHDuJgrcfOv" : "733OhaXQIHY7BKtY3vnSkn", // how you get the girl
+    "4L560fic5PaTuCFm6CviKa" : "4WBEj8TeGtRPNJdOmT3WJW", // this love
+    "6EwNJz8CuVsrsLvXprJ20Q" : "1ULabO0PEsdJekqVH6G10G", // i know places
+    "1NmVZsG18CzCAtw7rnV3yA" : "0lKUBmEyQfzsQHozyeXzES", // clean
+    "6RvRzl1YJTDnUvdOtV21IK" : "6HRsJu8vcnzYDN4t0570FY", // wonderland
+    "2KrOAg6FftbjgSKdd2a4rS" : "0TyGh27YQ5LknmiDhCzJiT", // you are in love
+    "0qAIiGFKLdV1xpNlEhjpq8" : "5M787RexsAiVYjQusM98CV", // new romantics
+
+    // 1989 (big machine)
+    "7aMftpNNJiQj1c2rllc8Dk" : "4WUepByoeqcedHoYhSNHRt", // welcome to ny
+    "1kHEuJRasudLhjvnbfc4yS" : "0108kcWLnn2HlH2kedi1gn", // blank space
+    "4D7kxLhSz7siCOw7KA5rQn" : "3Vpk1hfMAQme8VJ0SNRSkd", // style
+    "7CkJgmJ3RFU6jNXNHgRkBw" : "1OcSfkeCg9hRC2sFKB4IMJ", // out of the woods
+    "50Pu9HKAcPm8TFL2zDrouh" : "2k0ZEeAqzvYMcx9Qt5aClQ", // all you had to do was stay
+    "1CiVz2WkmZRwhU3A6J2Gjz" : "50yNTF0Od55qnHLxYsA5Pw", // shake it off
+    "3JPOspEefgmGT5Hyy11oQJ" : "3FxJDucHWdw6caWTKO5b23", // i wish you would
+    "4SHyS2ThjrS1iBGruKweCz" : "7oZONwFiFIErZcXAtTu7FY", // bad blood
+    "3ScVaMDQwaD8iumBYzcDvr" : "27exgla7YBw9DUNNcTIpjy", // wildest dreams
+    "1l6KjnsqP2atawKgBV49u3" : "733OhaXQIHY7BKtY3vnSkn", // how you get the girl
+    "5yCRSEPJXzjzl3Gov0YduA" : "4WBEj8TeGtRPNJdOmT3WJW", // this love
+    "1fDfzSgIEFWDuX383vRiAv" : "1ULabO0PEsdJekqVH6G10G", // i know places
+    "1s4CiRufJUyeyQT6HRmu7S" : "0lKUBmEyQfzsQHozyeXzES", // clean
+
+    // bad blood with kendrick
+    "6xsEAm6w9oMQYYg3jkEkMT" : "6qAcApH8obo8eqatCKUHd9",
+};
